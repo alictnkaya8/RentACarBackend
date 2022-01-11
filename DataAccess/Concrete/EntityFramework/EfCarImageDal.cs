07e943e1af467d0ad855e862d122c92afb65b96a
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-01-12 02:32:00 +0300

    Araba resimleri ve CRUD operasyonları eklendi

---
+++ DataAccess/Concrete/EntityFramework/EfCarImageDal.cs
@@ -0,0 +1,25 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarImageDal : EfEntityRepositoryBase<CarImage, CarRentalContext>, ICarImageDal
+    {
+        public List<CarImage> GetCarImages()
+        {
+            using (CarRentalContext context = new CarRentalContext())
+            {
+                var result = from c in context.Cars
+                             join i in context.CarImages on c.Id equals i.CarId
+                             select new CarImage { CarId = c.Id };
+                return result.ToList();
+            }
+        }
+    }
+}
